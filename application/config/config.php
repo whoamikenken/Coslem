6bce70d8b7a854ecd10e6a124218242a62e87067
Kennedy Hipolito <whoamikenken@gmail.com>
2023-05-09 12:44:59 +0800

    test

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'https://coslem.herokuapp.com/';
-$config['base_url'] = 'http://localhost:8080/coslem';
-// $config['base_url'] = 'http://localhost/Coslem';
+// ken Config
+// $config['base_url'] = 'http://localhost:8080/coslem';
+
+// Localhost
+$config['base_url'] = 'http://localhost/Coslem';
 
 /*
 |--------------------------------------------------------------------------
